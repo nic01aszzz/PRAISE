5cf8a2ce5786356ef33ec7404995214484349281
nic01aszzz <nicopiccart@hotmail.com>
2025-11-05 00:05:05 -0300

    Creada carpeta chess, empezado chess world y agregado commands.txt

---
+++ chess/chessworld.py
@@ -0,0 +1,16 @@
+"""
+Entorno: 
+Totalmente observable: el agente puede ver todo el tablero.
+Discreto: hay un número finito de acciones posibles para el agente. 
+Estático?: mientras el agente piensa su jugada, el tablero no se mueve.
+Estocástico: el movimiento del jugador aporta aleatoridad.
+
+
+Se tiene un tablero (matriz 8x8) con una posición inicial predeterminada, con las casillas nombradas 
+(nomenclatura estándar).
+Hay piezas, cada una con su comportamiento específico. 
+La idea es que el jugador juega contra el agente. 
+
+Futura implementación: que se vea en pantalla y que rote 180° en cada turno.
+
+"""
